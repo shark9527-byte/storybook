3bb270bdf1fb5b947dbcd32aec0a06d6a437db57
shark <xlshark92@gmail.com>
2026-05-18 18:20:30 +0800

    fix: add explicit flutter/gestures import for TapGestureRecognizer

--- lib/screens/reader_screen.dart
+++ lib/screens/reader_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:provider/provider.dart';
